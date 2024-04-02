935d785ba4d68657c9ceb5d89be85c6cb8114197
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-04-03 01:01:01 +0530

    changed file names and add loginerrordisplay.js

---
+++ public/scripts/loginerrordisplay.js
@@ -0,0 +1,36 @@
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get username and password from the form
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Send a POST request to the server
+    fetch("/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          window.location.href = "/home"; // Redirect to /home on successful login
+        } else {
+          // Display error message
+          // Change the text content of the button element
+          document.getElementById("login-button").textContent = data.message;
+
+          // Delay reverting the text content back to "Login" after 2 seconds
+          setTimeout(function () {
+            document.getElementById("login-button").textContent = "Login";
+          }, 2000);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
