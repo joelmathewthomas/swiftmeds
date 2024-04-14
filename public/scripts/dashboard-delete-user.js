cd94d34120d325885a501f6447ebdbb435159001
joelmathewthomas <0qx76r@proton.me>
2024-04-14 13:14:39 +0530

    delete user backend for dashboard half done

--- public/scripts/dashboard-delete-user.js
+++ public/scripts/dashboard-delete-user.js
@@ -14,3 +14,37 @@ document.addEventListener("DOMContentLoaded", function () {
     deleteUserPopup.style.display = "none";
   });
 });
+
+document
+  .getElementById("dashboard-delete-user-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var username = document.getElementById("dashboard-delete-username");
+
+    fetch("/dashboardDeleteuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          window.location.href = "/dashboard";
+        } else {
+          document.getElementById("dashboard-popup-delete").textContent =
+            data.message;
+          setTimeout(function () {
+            document.getElementById("dashboard-popup-delete").textContent =
+              "Delete";
+          }, 2000);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
