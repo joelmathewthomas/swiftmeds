6e05282f1ca9745b637f4f5a853d0b164fba1e2c
joelmathewthomas <0qx76r@proton.me>
2024-04-15 23:08:47 +0530

    added favicon

--- public/scripts/get-username.js
+++ public/scripts/get-username.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       if (data.loggedin) {
         document.getElementById("login-link").textContent = data.username;
-        console.log("User type:", data.type);
       } else {
-        console.log("User is not logged in");
       }
     })
     .catch((error) => console.error("Error fetching session data:", error));
