ece6af0dd2bd091c835ac3871d26f92bb6a5105f
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-04-16 17:51:48 +0530

    fixed medicine options toggle

---
+++ public/scripts/medicine_options.js
@@ -0,0 +1,44 @@
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  uploadForm.classList.remove("active");
+};
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  uploadForm.classList.remove("active");
+};
+
+let uploadForm = document.querySelector(".upload-form");
+const defaultBtn = document.querySelector(".default-btn");
+const customBtn = document.querySelector(".custom-btn");
+const fileName = document.querySelector(".file-name");
+
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_]+$/;
+
+function active() {
+  defaultBtn.click();
+}
+defaultBtn.addEventListener("change", function () {
+  if (this.value) {
+    let nameValue = this.value.match(regExp);
+    fileName.textContent = nameValue;
+  }
+});
+
+document.querySelector("#up-btn").onclick = () => {
+  uploadForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  uploadForm.classList.remove("active");
+};
