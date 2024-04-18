7f61e5d42cd6228c27e219270ac4ddd5cabbea68
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-04-19 01:29:32 +0530

    shows no appointments if no appointments

--- public/scripts/appointments.js
+++ public/scripts/appointments.js
@@ -4,47 +4,50 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.json())
     .then((data) => {
       if (data.type === "doctor") {
-        // Clear existing contents
-        document.querySelector("#appointmentContainer .collection").innerHTML =
-          "";
+        if (data.appointments.length > 0) {
+          // Clear existing contents
+          document.querySelector(
+            "#appointmentContainer .collection"
+          ).innerHTML = "";
 
-        // Fetch list of appointments
-        data.appointments.forEach((patient) => {
-          // Create list item element
-          const listItem = document.createElement("li");
-          listItem.classList.add("collection-item");
+          // Fetch list of appointments
+          data.appointments.forEach((patient) => {
+            // Create list item element
+            const listItem = document.createElement("li");
+            listItem.classList.add("collection-item");
 
-          // Create accept button
-          const acceptButton = document.createElement("a");
-          acceptButton.classList.add(
-            "waves-effect",
-            "waves-light",
-            "btn",
-            "acceptappointment"
-          );
-          acceptButton.textContent = " Accept";
+            // Create accept button
+            const acceptButton = document.createElement("a");
+            acceptButton.classList.add(
+              "waves-effect",
+              "waves-light",
+              "btn",
+              "acceptappointment"
+            );
+            acceptButton.textContent = " Accept";
 
-          // Create reject button
-          const rejectButton = document.createElement("a");
-          rejectButton.classList.add(
-            "waves-effect",
-            "waves-light",
-            "btn",
-            "rejectappointment"
-          );
-          rejectButton.textContent = "Reject";
+            // Create reject button
+            const rejectButton = document.createElement("a");
+            rejectButton.classList.add(
+              "waves-effect",
+              "waves-light",
+              "btn",
+              "rejectappointment"
+            );
+            rejectButton.textContent = "Reject";
 
-          // Add patient username
-          const patientText = document.createTextNode(patient);
-          listItem.appendChild(patientText);
-          listItem.appendChild(acceptButton);
-          listItem.appendChild(rejectButton);
+            // Add patient username
+            const patientText = document.createTextNode(patient);
+            listItem.appendChild(patientText);
+            listItem.appendChild(acceptButton);
+            listItem.appendChild(rejectButton);
 
-          // Add list item to the collection
-          document
-            .querySelector("#appointmentContainer .collection")
-            .appendChild(listItem);
-        });
+            // Add list item to the collection
+            document
+              .querySelector("#appointmentContainer .collection")
+              .appendChild(listItem);
+          });
+        }
       }
     })
     .catch((error) => console.error("Error fetching session data:", error));
