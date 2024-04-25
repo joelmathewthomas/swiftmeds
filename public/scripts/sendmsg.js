a20ecb85bf915157cbbcd217e293e0833493e9e6
Joel Mathew Thomas <90510078+joelmathewthomas@users.noreply.github.com>
2024-04-25 11:20:38 +0530

    added enter to send, alignet input field label

--- public/scripts/sendmsg.js
+++ public/scripts/sendmsg.js
@@ -20,6 +20,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  document
+    .getElementById("messageInput")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        const message = messageInput.value;
+
+        if (message !== "") {
+          socket.emit("sendclicked", {
+            sender: username,
+            recipient: chatWith,
+            message: message,
+          });
+          console.log(
+            "did socketemit sendclicked ",
+            username,
+            chatWith,
+            message
+          );
+          // Display the sent message
+          displaySentMsg(message);
+          // Clear the input field after sending the message
+          messageInput.value = "";
+        }
+      }
+    });
+
   closeButton.addEventListener("click", function () {
     socket.emit("closedchat", { recipient: chatWith });
     location.reload();
